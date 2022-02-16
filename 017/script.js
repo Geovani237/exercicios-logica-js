b57c519be57b51a0762f44f8605dfaa61f3c6c88
Geovani237 <94084031+Geovani237@users.noreply.github.com>
2022-02-16 09:21:00 -0300

    017 e 018 Finalizado

---
+++ 017/script.js
@@ -0,0 +1,9 @@
+let num1
+let num2
+console.log("Informe o 1 número")
+num1 = 3
+console.log("Informe o 2 número")
+num2 = 4
+if(num1 < num2){
+    console.log("o segundo número é o maior")
+}
---
+++ 018/script.js
@@ -0,0 +1,16 @@
+let nota1
+let nota2
+let media
+console.log("Informer a 1 nota")
+nota1 = 5
+console.log("Informe a 2 nota")
+nota2 = 5
+media = (nota1 + nota2) / 2
+
+if(media < 5){
+    console.log("Reprovado!")
+}
+if(media >= 5){
+    console.log("Aprovado!")
+}
+
